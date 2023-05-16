49fdd97f32084848a69262d01ea44d4b104700f8
Gaurav Garg <gaurav.garg@salsa.digital>
2023-05-16 07:32:25 +0530

    Fix build issues.

--- src/Commands/TideSationLocatorMigrationCommands.php
+++ src/Commands/TideSationLocatorMigrationCommands.php
@@ -5,12 +5,15 @@ namespace Drupal\tide_station_locator\Commands;
 use Drush\Commands\DrushCommands;
 
 /**
- * Class TideSationLocatorMigrationCommands.
+ * Defines the drush command for Tide station locator.
  *
  * @package Drupal\tide_station_locator\Commands
  */
 class TideSationLocatorMigrationCommands extends DrushCommands {
 
+  /**
+   * Class constructor.
+   */
   public function __construct() {
     parent::__construct();
   }
@@ -27,4 +30,5 @@ class TideSationLocatorMigrationCommands extends DrushCommands {
   public function importStations() {
     tide_station_locator_migrate_stations();
   }
+
 }
