099cedd70dd9077a5f13c85b996bc686e2e87cf6
Gaurav Garg <gaurav.garg@salsa.digital>
2023-05-18 09:58:01 +0530

    [VICPOLSL-27] Fix logic.

--- assets/redirects.php
+++ assets/redirects.php
@@ -5,32 +5,42 @@
  * File has all the redirects mentioned.
  */
 
-$content_vicpol = [
-  'https://www.police.vic.gov.au/location/?q=alexandra' => '/site-4/alexandra-police-station',
-  'https://www.police.vic.gov.au/location/?q=altona' => '/site-4/altona-police-station',
-  'https://www.police.vic.gov.au/location/?q=angelsea' => '/site-4/anglesea-police-station',
-  'https://www.police.vic.gov.au/location/?q=apollo-bay' => '/site-4/apollo-bay-police-station',
-  'https://www.police.vic.gov.au/location/?q=apsley' => '/site-4/apsley-police-station',
-  'https://www.police.vic.gov.au/location/?q=ararat' => '/site-4/ararat-police-station',
-  'https://www.police.vic.gov.au/location/?q=avoca' => '/site-4/avoca-police-station',
-  'https://www.police.vic.gov.au/location/?q=avondale-heights' => '/site-4/avondale-heights-police-station',
-  'https://www.police.vic.gov.au/location/?q=axedale' => '/site-4/axedale-police-station',
-  'https://www.police.vic.gov.au/location/?q=bacchus-marsh' => '/site-4/bacchus-marsh-police-station',
-  'https://www.police.vic.gov.au/location/?q=bairnsdale' => '/site-4/bairnsdale-police-station',
-  'https://www.police.vic.gov.au/location/?q=ballan' => '/site-4/ballan-police-station',
-  'https://www.police.vic.gov.au/location/?q=ballarat' => '/site-4/ballarat-police-station',
-  'https://www.police.vic.gov.au/location/?q=balmoral' => '/site-4/balmoral-police-station',
-  'https://www.police.vic.gov.au/location/?q=bannockburn' => '/site-4/bannockburn-police-station',
-  'https://www.police.vic.gov.au/location/?q=bayside' => '/site-4/bayside-police-station',
-  'https://www.police.vic.gov.au/location/?q=beaufort' => '/site-4/beaufort-police-station',
-  'https://www.police.vic.gov.au/location/?q=beeac' => '/site-4/beeac-police-station',
-  'https://www.police.vic.gov.au/location/?q=beechworth' => '/site-4/beechworth-police-station',
-  'https://www.police.vic.gov.au/location/?q=belgrave' => '/site-4/belgrave-police-station',
-  'https://www.police.vic.gov.au/location/?q=bellarine' => '/site-4/bellarine-police-station',
-  'https://www.police.vic.gov.au/location/?q=benalla' => '/site-4/benalla-police-station',
-  'https://www.police.vic.gov.au/location/?q=bendigo' => '/site-4/bendigo-police-station',
-  'https://www.police.vic.gov.au/location/?q=bendoc' => '/site-4/bendoc-police-station',
-  'https://www.police.vic.gov.au/location/?q=bethanga' => '/site-4/bethanga-police-station',
-  'https://www.police.vic.gov.au/location/?q=beulah' => '/site-4/beulah-police-station',
-  'https://www.police.vic.gov.au/location/?q=birchip' => '/site-4/birchip-police-station',
-];
+/**
+ * Helper method to get all the redirects.
+ *
+ * @return array
+ *   Redirects array.
+ */
+function _tide_station_locator_redirects() {
+  $content_vicpol = [
+    'https://www.police.vic.gov.au/location/?q=alexandra' => '/site-4/alexandra-police-station',
+    'https://www.police.vic.gov.au/location/?q=altona' => '/site-4/altona-police-station',
+    'https://www.police.vic.gov.au/location/?q=angelsea' => '/site-4/anglesea-police-station',
+    'https://www.police.vic.gov.au/location/?q=apollo-bay' => '/site-4/apollo-bay-police-station',
+    'https://www.police.vic.gov.au/location/?q=apsley' => '/site-4/apsley-police-station',
+    'https://www.police.vic.gov.au/location/?q=ararat' => '/site-4/ararat-police-station',
+    'https://www.police.vic.gov.au/location/?q=avoca' => '/site-4/avoca-police-station',
+    'https://www.police.vic.gov.au/location/?q=avondale-heights' => '/site-4/avondale-heights-police-station',
+    'https://www.police.vic.gov.au/location/?q=axedale' => '/site-4/axedale-police-station',
+    'https://www.police.vic.gov.au/location/?q=bacchus-marsh' => '/site-4/bacchus-marsh-police-station',
+    'https://www.police.vic.gov.au/location/?q=bairnsdale' => '/site-4/bairnsdale-police-station',
+    'https://www.police.vic.gov.au/location/?q=ballan' => '/site-4/ballan-police-station',
+    'https://www.police.vic.gov.au/location/?q=ballarat' => '/site-4/ballarat-police-station',
+    'https://www.police.vic.gov.au/location/?q=balmoral' => '/site-4/balmoral-police-station',
+    'https://www.police.vic.gov.au/location/?q=bannockburn' => '/site-4/bannockburn-police-station',
+    'https://www.police.vic.gov.au/location/?q=bayside' => '/site-4/bayside-police-station',
+    'https://www.police.vic.gov.au/location/?q=beaufort' => '/site-4/beaufort-police-station',
+    'https://www.police.vic.gov.au/location/?q=beeac' => '/site-4/beeac-police-station',
+    'https://www.police.vic.gov.au/location/?q=beechworth' => '/site-4/beechworth-police-station',
+    'https://www.police.vic.gov.au/location/?q=belgrave' => '/site-4/belgrave-police-station',
+    'https://www.police.vic.gov.au/location/?q=bellarine' => '/site-4/bellarine-police-station',
+    'https://www.police.vic.gov.au/location/?q=benalla' => '/site-4/benalla-police-station',
+    'https://www.police.vic.gov.au/location/?q=bendigo' => '/site-4/bendigo-police-station',
+    'https://www.police.vic.gov.au/location/?q=bendoc' => '/site-4/bendoc-police-station',
+    'https://www.police.vic.gov.au/location/?q=bethanga' => '/site-4/bethanga-police-station',
+    'https://www.police.vic.gov.au/location/?q=beulah' => '/site-4/beulah-police-station',
+    'https://www.police.vic.gov.au/location/?q=birchip' => '/site-4/birchip-police-station',
+  ];
+
+  return $content_vicpol;
+}
