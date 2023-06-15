9ffba39ca2dacf87bd166fa607985629b3538654
Suchi <gargsuchi@gmail.com>
2023-06-16 09:50:32 +1000

    Fixed non breaking apace in accessibility.

--- src/StationAPIConnector.php
+++ src/StationAPIConnector.php
@@ -273,6 +273,8 @@ class StationAPIConnector {
           if (isset($accessibility_terms_array) && is_array($accessibility_terms_array)) {
             foreach ($accessibility_terms_array as $key1 => $item) {
               $trimmed_item = trim($item);
+              // Getting rif od non-breaking space.
+              $trimmed_item = trim($trimmed_item, " \t\n\r\0\x0B\xc2\xa0");
               if ($trimmed_item === "") {
                 continue;
               }
